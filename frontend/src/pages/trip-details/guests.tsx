8ca21a0ee3f3fd3156fa7f01352305a95b25ee34
Victor Matheus <victormatheus507@gmail.com>
2024-07-13 18:56:41 -0300

    GET API

--- frontend/src/pages/trip-details/guests.tsx
+++ frontend/src/pages/trip-details/guests.tsx
@@ -1,36 +1,53 @@
-import { CircleDashed, UserCog } from 'lucide-react';
+import { CheckCircle2, CircleDashed, UserCog } from 'lucide-react';
 import { Button } from '../../components/button';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
+
+interface Participants {
+  id: string;
+  name: string | null;
+  email: string;
+  is_confirmed: boolean;
+}
 
 export function Guests() {
+  const { tripId } = useParams();
+  const [participants, setParticipants] = useState<Participants[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/participants`)
+      .then((resposnse) => setParticipants(resposnse.data.participants));
+  }, [tripId]);
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl text-zinc-300">Convidados</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between">
-          <div className="space-y-1.5 gap-4">
-            <span className="block font-medium text-zinc-100">
-              Jessica White
-            </span>
+        {participants.map((particiapant, index) => {
+          return (
+            <div
+              key={particiapant.id}
+              className="flex items-center justify-between"
+            >
+              <div className="space-y-1.5 gap-4">
+                <span className="block font-medium text-zinc-100">
+                  {particiapant.name ?? `Convidado ${index}`}
+                </span>
 
-            <span className="block font-sm text-zinc-400 truncate">
-              jessica.white44@yahoo.com
-            </span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-        </div>
-
-        <div className="flex items-center justify-between">
-          <div className="space-y-1.5 gap-4">
-            <span className="block font-medium text-zinc-100">
-              Dr. Rita Pacocha
-            </span>
-
-            <span className="block font-sm text-zinc-400 truncate">
-              lacy.stiedemann@gmail.com
-            </span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-        </div>
+                <span className="block font-sm text-zinc-400 truncate">
+                  {particiapant.email}
+                </span>
+              </div>
+              {particiapant.is_confirmed ? (
+                <CheckCircle2 className="text-green-400 size-5 shrink-0" />
+              ) : (
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              )}
+            </div>
+          );
+        })}
       </div>
 
       <Button size="full" variant="secondary">
