4781528c9f677b386afe6c7d6cf0f339241c5cbc
Victor Matheus <victormatheus507@gmail.com>
2024-07-14 15:53:54 -0300

    confirm participant

--- backend/src/routes/confirm-trip.ts
+++ backend/src/routes/confirm-trip.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import nodemailer from 'nodemailer';
 import { z } from 'zod';
-import { prisma } from '../lib/prisma';
 import { dayjs } from '../lib/dayjs';
 import { getMailClient } from '../lib/mail';
-import nodemailer from 'nodemailer';
+import { prisma } from '../lib/prisma';
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -56,7 +56,7 @@ export async function confirmTrip(app: FastifyInstance) {
 
       await Promise.all(
         trip.participants.map(async (participant) => {
-          const confirmatinoLink = `http://localhost:3333/trips/${trip.id}/confirm/${participant.id}`;
+          const confirmatinoLink = `http://localhost:3333/participants/${participant.id}/confirm`;
           const message = await mail.sendMail({
             from: {
               name: 'Equipe plann.er',
